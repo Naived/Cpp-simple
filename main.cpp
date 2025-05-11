5cfdcc5077444789cdf70e617b4378aeedca1403
Nagami <Devinbranwen@students.amikom.ac.id>
2025-05-12 02:34:23 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Counting from 1 to 5 using a loop:" << std::endl;
+
+    for (int i = 1; i <= 5; ++i) {
+        std::cout << "Number: " << i << std::endl;
+    }
+
+    return 0;
+}
